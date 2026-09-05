1c50d656c60c7ec82d980f9a7ffc1ce05983e80d
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-09-05 14:46:35 +0200

    chore: add compose

--- jury/src/test/java/pl/ahlebowicz/jury/TestcontainersConfiguration.java
+++ jury/src/test/java/pl/ahlebowicz/jury/TestcontainersConfiguration.java
@@ -13,13 +13,13 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:4.3.1"));
     }
 
     @Bean
     @ServiceConnection
     PostgreSQLContainer postgresContainer() {
-        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:18.6-alpine"));
     }
 
 }
